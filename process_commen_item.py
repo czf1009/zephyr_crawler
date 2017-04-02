d9d092951e0aff047bb8bdb25d9e40d88132fb7c
Zephyr <365964325@qq.com>
2017-04-02 10:51:05 +0800

    Add process jd item

--- process_commen_item.py
+++ process_commen_item.py
@@ -20,6 +20,32 @@ class CommenItem(object):
         print u'process_commen_item耗时：' + str(time.time()-self.t) + u' 秒'
 
     ############testing   limit just get good lable item
+
+    def process_item(self):
+        good = ''
+        items = self.item_get()
+        print '读取数据完成，开始处理。'
+        items_len = len(items)
+        print '数据总量为：',items_len
+        for index,item in enumerate(items,1):
+            if index%1000 == 0:
+                print '进度： ',items_len,'/',index
+            if item[1] == 'good':
+                if not good:
+                    good = CommenGoodItem(self.cur,self.conn)
+                item_body = json.loads(item[2])
+                good.item_insert(item_body,item[3])
+            elif item[1] == 'jd':
+                if not jd:
+                    jd = CommenJdItem(self.cur,self.conn)
+                item_body = json.loads(item[2])
+                good.item_insert(item_body,item[3])
+            else:
+                print 'Error lable: ',item[1]
+                print 'id: ',item['id'],'\n\n'
+        # self.item_del(len(items))
+        self.close_process()
+
     #Get items
     def item_get(self):
         if self.cur.execute('select id,lable,body,date from commen where lable=\'good\' order by date;'):
@@ -27,53 +53,30 @@ class CommenItem(object):
         else:
             print '\n\ninsert data ERROR!!!'    
 
-    def process_item(self):
-    	good = ''
-        items = self.item_get()
-	print '读取数据完成，开始处理。'
-	items_len = len(items)
-	print '数据总量为：',items_len
-        for index,item in enumerate(items,1):
-	    if index%1000 == 0:
-	    	print '进度： ',items_len,'/',index
-            if item[1] == 'good':
-                if not good:
-                    good = CommenGoodItem(self.cur,self.conn)
-                item_body = json.loads(item[2])
-                good.item_insert(item_body,item[3])
-            else:
-                print 'Error lable: ',item[1]
-                print 'id: ',item['id'],'\n\n'
-	self.item_del(len(items))
-        self.close_process()
-
     #  Now just delete good lable item
     def item_del(self,items_len):
-	if self.cur.execute('delete from commen where lable=\'good\'') == items_len:
-	    self.conn.commit()
-	    print "\nDelete commen complite.\n"
-	else:
-	    print "Items_len: " , items_len
-	    print "Error number when delete commen!!\n"
+        if self.cur.execute('delete from commen where lable=\'good\';') == items_len:
+            self.conn.commit()
+            print "\nDelete commen complite.\n"
+        else:
+            print "Items_len: " , items_len
+            print "Error number when delete commen!!\n"
 
 
-# Good processor
+######################### Good processor
 class CommenGoodItem(object):
     def __init__(self,cur,conn):
         self.cur = cur
         self.conn = conn
 
-    def item_del(self):
-        pass
-
     def item_insert(self,item,date):
         #process item type & specia charactor
-    	item = self.item_initial(item)
+        item = self.item_initial(item)
 
         if not self.is_exist(item['outerid']):
             self.insert_good(item,date)
-	else:
-	    self.update_good_date(item,date)
+        else:
+            self.update_good_date(item,date)
 
         self.insert_price_stock(item,date)
         return
@@ -81,34 +84,32 @@ class CommenGoodItem(object):
     def item_initial(self,item):
         #Initial value type
         item['outerid'] = int(item['outerid'])
-	ids = ['categoryID','subCategoryID','ThirdCategoryID']
-	for id in ids:
-	    if id in item.keys():
-	    	if item[id]:
-       	            item[id] = int(item[id])
-	        else:
-	            item[id] = 0
+        ids = ['categoryID','subCategoryID','ThirdCategoryID']
+        for id in ids:
+            if id in item.keys():
+                if item[id]:
+                    item[id] = int(item[id])
+                else:
+                    item[id] = 0
         item['price'] = float(item['price'])
         item['value'] = float(item['value'])
         item['vipprice'] = float(item['vipprice'])
         item['svipprice'] = float(item['svipprice'])
-	if item['stock']:
- 	    item['stock'] = int(item['stock'])
-	else:
-	    item['stock'] = 0
-	    #print '\n\n',item['outerid'],item['name']
-	    #print item['stock'],'\n\n'
-	#Translate special caractor
-	names = ['name','brand','categoryName','subCategoryName','ThridCategoryName','description']
-	for name in names:
-	    if name in item.keys():
-	        item[name] = item[name].replace("\'","\\\'")
-	return item
+        if item['stock']:
+            item['stock'] = int(item['stock'])
+        else:
+            item['stock'] = 0
+        #Translate special caractor
+        names = ['name','brand','categoryName','subCategoryName','ThridCategoryName','description']
+        for name in names:
+            if name in item.keys():
+                item[name] = item[name].replace("\'","\\\'")
+        return item
 
 
     #insert good
     def insert_good(self,item,date):
-    	#print('',item['name'],item['categoryName'])
+        #print('',item['name'],item['categoryName'])
         brand_id = self.check_brand(item['brand'])[0]
         self.check_category(item['categoryID'],item['categoryName'])
         self.check_category(item['subCategoryID'],item['subCategoryName'])
@@ -125,7 +126,7 @@ class CommenGoodItem(object):
                 `image`,
                 `description`,
                 `date_create`,
-		`date_last`
+                `date_last`
             ) values(%d,\'%s\',%d,\'%s\',%d,%d,%d,\'%s\',\'%s\',\'%s\',\'%s\');''' 
             % (item['outerid'],item['name'],brand_id,item['loc'],item['categoryID'],item['subCategoryID'],item['ThirdCategoryID'],item['image'],item['description'],date,date)
         ):
@@ -134,11 +135,9 @@ class CommenGoodItem(object):
             print 'insert good error\n'
 
     def update_good_date(self,item,date):
-    	#print('UPDATE `app_test`.`good` SET `date_last`=\'%s\' WHERE `good_id`=%d;' % (date,item['outerid']))
- 
-    	self.cur.execute('UPDATE `app_test`.`good` SET `date_last`=\'%s\' WHERE `good_id`=%d;' % (date,item['outerid']))
-	self.conn.commit()
-	return
+        self.cur.execute('UPDATE `app_test`.`good` SET `date_last`=\'%s\' WHERE `good_id`=%d;' % (date,item['outerid']))
+        self.conn.commit()
+        return
 
     #insert price&stock
     def insert_price_stock(self,item,date):
@@ -175,18 +174,55 @@ class CommenGoodItem(object):
                 print 'insert brand error\n'
             self.cur.execute('select brand_id from brand where brand_name = \'%s\';' % brand_name)
         return self.cur.fetchone()
-	
+        
 
     #check is exist in category
     def check_category(self, category_id,category_name):
         if not self.cur.execute('select * from category where category_id = %d;' % category_id):
-            #print('\ninsert into category(`category_id`,`category_name`) values(%d,\'%s\');\n' % (category_id,category_name))
-	    #print('',category_name)
             if self.cur.execute('insert into category(`category_id`,`category_name`) values(%d,\'%s\');' % (category_id,category_name)):
                 self.conn.commit()
             else:
                 print 'insert category error\n'
         return
 
-commenItem = CommenItem()
-commenItem.process_item()
+################################ jd processor
+class CommenJdItem(object):
+    def __init__(self,cur,conn):
+        self.cur = cur
+        self.conn = conn
+
+
+    def item_insert(self,item,date):
+        #process item type & specia charactor
+        item = self.item_initial(item)
+        self.insert_jd(item,date)
+        return
+    
+    def item_initial(self,item):
+        #Initial value type
+        item['wareId'] = int(item['wareId'])
+        item['jdPrice'] = float(item['jdPrice'])
+        item['wname'] = item['wname'].replace("\'","\\\'")
+        return item
+
+
+    #insert jd
+    def insert_jd(self,item,date):
+        if self.cur.execute('''
+             insert into good(
+                `ware_id`,
+                `wname`,
+                `jd_price`,
+                `date`
+            ) values(%d,\'%s\',%f,\'%s\');''' 
+            % (item['wareId'],item['wname'],item['jdPrice'],date)
+        ):
+            self.conn.commit()
+        else:
+            print 'insert jd error\n'
+
+
+
+if __name__ == '__main__':
+    commenItem = CommenItem()
+    commenItem.process_item()
