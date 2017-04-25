ec4d6c58b4548e5a0983f04724d3b77da85c56de
Zephyr <czf1009@126.com>
2017-04-25 17:03:43 +0800

    change test_thread.sh to abuyun proxy test

--- test_threads.sh
+++ test_threads.sh
@@ -3,12 +3,19 @@
 for i in 1 2 3 4 5
 do
 	echo '################'$i'times#############'>> test.log
-	for j in 256 192 128 64 32 16 8 4 1
+	echo "start time: "`date` >> test.log
+	sh push_redis-key.sh
+    # scrapy crawl banggo -L INFO -s LOG_FILE=banggo.log >> test.log
+	sleep 60;
+	while true
 	do
-		concurent_request=`grep -E "CONCURRENT_REQUESTS =" zephyr_crawler/settings.py`
-		sed -i "s/${concurent_request}/CONCURRENT_REQUESTS = "${j}"/" zephyr_crawler/settings.py
-		echo '########Thread number'$j>> banggo.log
-    	scrapy crawl banggo -L INFO -s LOG_FILE=banggo.log >> test.log
-		grep -E 'request_count|#####' banggo.log|tail -n 1 >> test.log
-    done
+		if [ `redis-cli EXISTS banggo:requests` -eq 0 ];then
+			break;
+		else
+			sleep 1;
+		fi
+	done
+	echo "banggo:dupefilter: "`redis-cli SCARD banggo:dupefilter` >> test.log
+	redis-cli FLUSHALL
+	echo "end time: "`date` >> test.log
 done
