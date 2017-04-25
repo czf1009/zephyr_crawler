d1043bc50b6e72d6aa65d0deb83132cc7c30d88c
Zephyr <czf1009@126.com>
2017-04-25 17:19:00 +0800

    change launch

--- launch.py
+++ launch.py
@@ -1,5 +1,6 @@
 if __name__ == '__main__':
     from scrapy import cmdline
     # cmdline.execute("scrapy crawl jd -L INFO".split())
-    cmdline.execute("scrapy crawl jd_comment".split())
+    # cmdline.execute("scrapy crawl jd_comment".split())
+    cmdline.execute("scrapy crawl banggo -L INFO -s LOG_FILE=banggo.log >> test.log".split())
     # cmdline.execute("scrapy crawl banggo -L WARNING -s LOG_FILE=banggo.log".split())
