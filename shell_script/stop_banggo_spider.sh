e2c1c4c7741f9bd73b1a3ea239e8bb522862e16d
Zephyr <czf1009@126.com>
2017-04-28 10:17:42 +0800

    add stop_banggo_spider.sh

---
+++ shell_script/stop_banggo_spider.sh
@@ -0,0 +1,19 @@
+scrapys=`ps -ef|grep scrapy|grep -v grep|awk '{print $2}'`
+logname="banggo_"$(date +%Y%m%d_%H%M%S)".log"
+if [ $scrapys ];then
+	kill ${scrapys[0]}
+	for i in {1..15}
+	do
+		if [ `ps -ef|grep scrapy|grep -v grep|awk '{print $2}'` ];then
+			sleep 1;
+		elif [ $i -eq 15 ];then
+			echo 'kill scrapy failed! please check is scrapy runing.'
+			exit
+		else
+			echo 'kill scrapy process success.'
+			break
+		fi
+	done
+else
+	echo "No scrapy runing."
+fi
